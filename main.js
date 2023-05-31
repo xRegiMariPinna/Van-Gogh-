bec7836295ec886a2c0b40a425fc5b50c9d59fb1
xRegiMariPinna <110124921+xRegiMariPinna@users.noreply.github.com>
2023-05-31 18:12:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var noseX = 0;
+var noseY = 0;
+var difference = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+var image="";
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    image = loadImage('vangoghoriginalUwU.webp');
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+ }
+}
+
+function draw() {
+    background('#969A97');
+    document.getElementById("square_side").innerHTML = "Largura e altura serão = " + difference + "px";
+    image(image, noseX - 200, noseY - 150, difference, difference);
+
+}
